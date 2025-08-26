0807c5e56bb0cc4ec0144ac82ab08976bff6f895
NJY <to73005981@gamil.com>
2025-08-26 13:07:52 +0900

    init: Next.js frontend (Tailwind v4)

---
+++ app/page.tsx
@@ -0,0 +1,10 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main className="min-h-screen flex items-center justify-center bg-gray-100">
+      <h1 className="text-4xl font-bold text-blue-600">파이썬버리고 next.js로 다시시작!</h1>
+    </main>
+  );
+}
+
